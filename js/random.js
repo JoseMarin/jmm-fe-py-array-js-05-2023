fd9e57494f4d80ec2795fde893fa30b9226f0ae7
JoseMarin <jmarinmalagon@gmail.com>
2023-05-25 16:55:01 +0200

    numero random 0 - 9

--- js/random.js
+++ js/random.js
@@ -1,6 +1,10 @@
 // Calcular un numero random entre 1 y 10
-//Math.floor(Math.random() * (max - min)) + min;
+//let numero = Math.floor(Math.random()*(10 - 1)) + 1;
 
-let numero = Math.floor(Math.random() * (10 - 1)) + 1;
+let numero = Math.floor(Math.random()*(9));
 
 console.log(numero);
+
+let array = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+
+console.log(array[numero]);
